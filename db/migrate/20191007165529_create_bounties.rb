83d4f1130c63c97476c4d2ecb627ad9c479fb663
Brian Velez <bvez94@gmail.com>
2019-10-07 10:11:06 -0700

    generated bounties model

---
+++ db/migrate/20191007165529_create_bounties.rb
@@ -0,0 +1,13 @@
+class CreateBounties < ActiveRecord::Migration[5.2]
+  def change
+    create_table :bounties do |t|
+      t.string :tags, array: true
+      t.integer :bounty_amount
+      t.string :link 
+      t.datetime :bounty_closes_date
+      t.belongs_to :user
+
+      t.timestamps
+    end
+  end
+end
