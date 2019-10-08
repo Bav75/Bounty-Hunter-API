d92e1d98d60c93a8719e458866d6d2641168e1ec
Brian Velez <bvez94@gmail.com>
2019-10-08 12:06:34 -0700

    implemented login action for sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,19 @@
 class SessionsController < ApplicationController
 
     def login
-        binding.pry
+        # binding.pry
+        user = User.find_by(username: params[:user][:username])
+        if user && user.authenticate(params[:user][:password])
+            render json: UserSerializer.new(user)
+        else
+            render json: {errors: "Incorrect login credentials. Please try again.", status: 400}
+        end
+    end
+
+    private
+
+    def user_params
+     params.require(:user).permit(:username, :password)
     end
 
 end
