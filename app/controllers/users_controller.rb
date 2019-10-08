96f603bced7b71e270c5e1bc110673331e51aa29
Brian Velez <bvez94@gmail.com>
2019-10-08 10:06:22 -0700

    building out create users controller action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,26 @@
 class UsersController < ApplicationController
 
     def create
-        binding.pry
+        # binding.pry
+
+        if User.find_by(username: params[:user][:username])
+            render json: {errors: "This username already exists. Please try another name.", status: 400}
+        else
+            user = User.new(user_params)
+
+            if user.save
+                render json: 
+        end
     end
 
     def update      
     end
 
-    
+    private
+
+    def user_params
+     params.require(:user).permit(:username, :password)
+    end
+ 
 
 end
