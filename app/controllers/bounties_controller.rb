8ca97d3e4de2aae1958f9545d1d2341eaefa7ab3
Brian Velez <bvez94@gmail.com>
2019-10-09 12:28:51 -0700

    added helper method to bounty class. continue building out create action

--- app/controllers/bounties_controller.rb
+++ app/controllers/bounties_controller.rb
@@ -1,8 +1,17 @@
 class BountiesController < ApplicationController
 
     def create
-      binding.pry
-      id = params[:search][:userId]
+    #   binding.pry
+      userId = params[:search][:userId]
+
+      link = params[:search][:link]
+      tags = params[:search][:tags]
+      bounty_amount = params[:search][:bounty_amount]
+      bounty_closes_date = params[:search][:bounty_closes_date]
+
+      user = User.find_by(id: userId)
+
+      
     end
 
 end
--- app/models/bounty.rb
+++ app/models/bounty.rb
@@ -1,3 +1,10 @@
 class Bounty < ApplicationRecord
     belongs_to :user
+
+
+    # finds existing bounties for a given user and bounty
+    def self.find_duplicates(title, user)
+        self.where(user: user, title: title)
+    end
+
 end
