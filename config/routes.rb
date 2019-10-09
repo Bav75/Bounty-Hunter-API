e638365a27ea2350d7e7f575f619ef7773cd221e
Brian Velez <bvez94@gmail.com>
2019-10-09 12:35:09 -0700

    revised bounty routes: added index and removed show and update

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  resources :bounties, only: [:create, :update, :show]
+  resources :bounties, only: [:index, :create]
   resources :users, only: [:create, :update, :show]
   post '/login', to: 'sessions#login'
 
